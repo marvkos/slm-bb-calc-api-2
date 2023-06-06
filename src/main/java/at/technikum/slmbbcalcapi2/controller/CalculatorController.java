736e29ff91f9e0bfbd33779087460c629c3aed27
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2023-06-06 19:30:24 +0200

    add CalculatorController and add function

---
+++ src/main/java/at/technikum/slmbbcalcapi2/controller/CalculatorController.java
@@ -0,0 +1,18 @@
+package at.technikum.slmbbcalcapi2.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @RequestMapping("/calc/add")
+    public int add(
+        @RequestParam int a,
+        @RequestParam int b
+    ) {
+        return a + b;
+    }
+}
